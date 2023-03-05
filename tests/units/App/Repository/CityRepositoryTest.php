b463aff12686292720bab4c0cb5a2be3e6d1c313
BenTeboul <benj.teboul@gmail.com>
2023-03-05 16:05:48 +0100

    :sparkles: 5B. Tests unitaires

--- tests/units/App/Repository/CityRepositoryTest.php
+++ tests/units/App/Repository/CityRepositoryTest.php
@@ -2,6 +2,7 @@
 
 namespace units\App\Repository;
 
+use App\Entity\City;
 use App\Repository\CityRepository;
 use PHPUnit\Framework\TestCase;
 
@@ -20,5 +21,30 @@ class CityRepositoryTest extends TestCase
             CityRepository::class,
             $this->instance
         );
+    }
+    public function testFetchByDepartmentIdReturnsExpectedCities(){
+        $cities = $this->instance->fetchByDepartmentId(105);
+
+        $this->assertCount(1, $cities);
+        $this->assertInstanceOf(City::class, $cities[0]);
+        $this->assertSame('Paris', $cities[0]->getName());
+    }
+    public function testFetchByDepartmentIdWithUnknownDepartmentIdReturnsEmptyArray()
+    {
+        $actualCities = $this->instance->fetchByDepartmentId(999);
+
+        $this->assertEmpty($actualCities);
+    }
+
+    public function testFetchByDepartmentIdWithInvalidId(): void
+    {
+        $this->assertSame([], $this->instance->fetchByDepartmentId(999));
+    }
+
+    public function testFetchByDepartmentIdWithValidId(): void
+    {
+        $cities = $this->instance->fetchByDepartmentId(124);
+        $this->assertInstanceOf(City::class, $cities[0]);
+        $this->assertCount(47, $cities);
     }
 }
